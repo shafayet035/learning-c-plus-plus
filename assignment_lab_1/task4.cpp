0ccb7bab499b63b29528f217991ecc0c2302098a
shafayet035 <shafayet035@gmail.com>
2022-06-08 19:19:23 +0600

    test

--- assignment_lab_1/task4.cpp
+++ assignment_lab_1/task4.cpp
@@ -4,13 +4,13 @@ using namespace std;
 int main() {
     int number1, number2, number3, sum;
 
-    cout << "Please Enter a Number 1: ";
+    cout << "Please Enter Number 1: ";
     cin >> number1;
 
-    cout << "Please Enter a Number 2: ";
+    cout << "Please Enter Number 2: ";
     cin >> number2;
 
-    cout << "Please Enter a Number 3: ";
+    cout << "Please Enter Number 3: ";
     cin >> number3;
 
     sum = number1 + number2 + number3;
